d7f9986eda254cafbf4feb16cfbe39af0d5d8980
Rodolphe Renaux <rrenaux@idobs.net>
2022-03-20 22:36:52 +0100

    add dragonborn

--- src/races/index.ts
+++ src/races/index.ts
@@ -1,8 +1,11 @@
 import { SourceKeys } from "../sources";
-import { Abilities, Languages, Sizes, SourceArray } from "../utils";
+import { Languages, Sizes, SourceArray } from "../utils";
+
+export type Feature = {};
 
 export type Race = {
   name: string;
+  description?: string;
   regExpSearch: RegExp;
   plural: string;
   source: SourceArray;
@@ -28,10 +31,15 @@ export type Race = {
       ]
     | [[amount: number, nb: number]];
   traits: { [name: string]: string };
+  features?: {
+    [name: string]:
+      | Feature
+      | ((currentCharacter: { race: Race & { level: number } }) => Feature);
+  };
 };
 
-export const races: Race[] = [
-  {
+export const races: { [key: string]: Race } = {
+  human: {
     name: "Human",
     regExpSearch: /human/i,
     source: [
@@ -67,4 +75,90 @@ export const races: Race[] = [
       },
     ],
   },
-];
+  dragonborn: {
+    regExpSearch: /dragonborn/i,
+    name: "Dragonborn",
+    description:
+      "Your draconic heritage manifests in a variety of traits you share with other dragonborn.",
+    source: [
+      [SourceKeys.SRD, 5],
+      [SourceKeys.P, 34],
+    ],
+    plural: "Dragonborn",
+    size: Sizes.Medium,
+    speed: {
+      walk: { spd: 30, enc: 20 },
+    },
+    languageProfs: [Languages.Common, Languages.Draconic, 0],
+    age: " reach adulthood by 15 and live around 80 years",
+    height: ' stand well over 6 feet tall (5\'6" + 2d8")',
+    weight: " weigh around 240 lb (175 + 2d8 \xD7 2d6 lb)",
+    heightMetric: " stand well over 1,8 metres tall (170 + 5d8 cm)",
+    weightMetric: " weigh around 110 kg (80 + 5d8 \xD7 4d6 / 10 kg)",
+    scorestxt: "+2 Strength, +1 Charisma",
+    scores: [2, 0, 0, 0, 0, 1],
+    traits: {
+      "Draconic Ancestry":
+        "You have draconic ancestry. Choose one type of dragon from the Draconic Ancestry table. Your breath weapon and damage resistance are determined by the dragon type, as shown in the table.",
+      "Breath Weapon":
+        "You can use your action to exhale destructive energy. Your draconic ancestry determines the size, shape, and damage type of the exhalation.\n" +
+        "When you use your breath weapon, each creature in the area of the exhalation must make a saving throw, the type of which is determined by your draconic ancestry. The DC for this saving throw equals 8 + your Constitution modifier + your proficiency bonus. A creature takes 2d6 damage on a failed save, and half as much damage on a successful one. The damage increases to 3d6 at 6th level, 4d6 at11th level, and 5d6 at 16th level.\n" +
+        "After you use your breath weapon, you can’t use it again until you complete a short or long rest.",
+      "Damage Resistance":
+        "You have resistance to the damage type associated with your draconic ancestry.",
+      Languages:
+        "You can speak, read, and write Common and Draconic. Draconic is thought to be one of the oldest languages and is often used in the study of magic. The language sounds harsh to most other creatures and includes numerous hard consonants and sibilants.",
+    },
+    features: {
+      "draconic ancestry": (currentCharacter) => ({
+        name: "Draconic Ancestry",
+        limfeaname: "Breath Weapon",
+        minlevel: 1,
+        usages: 1,
+        dmg:
+          (currentCharacter.race.level < 6
+            ? 2
+            : currentCharacter.race.level < 11
+            ? 3
+            : currentCharacter.race.level < 16
+            ? 4
+            : 5) + "d6",
+        recovery: "short rest",
+        action: [["action", ""]],
+        range: /black|blue|brass|bronze|copper/i.test(
+          currentCharacter.race.name
+        )
+          ? "5-ft \xD7 30-ft line"
+          : "15-ft cone",
+        description:
+          "Hits all in area; Dex save, success - half damage; Usable only once per short rest".replace(
+            /(dex|con) save/i,
+            (/silver|white|green/i.test(currentCharacter.race.name)
+              ? "Con"
+              : "Dex") + " save"
+          ),
+        dmgtype: /black|copper/i.test(currentCharacter.race.name)
+          ? "Acid"
+          : /blue|bronze/i.test(currentCharacter.race.name)
+          ? "Lightning"
+          : /brass|gold|red/i.test(currentCharacter.race.name)
+          ? "Fire"
+          : /green/i.test(currentCharacter.race.name)
+          ? "Poison"
+          : "Cold",
+      }),
+    },
+    variants: [
+      { name: "Black dragonborn" },
+      { name: "Blue dragonborn" },
+      { name: "Brass dragonborn" },
+      { name: "Bronze dragonborn" },
+      { name: "Copper dragonborn" },
+      { name: "Gold dragonborn" },
+      { name: "Green dragonborn" },
+      { name: "Red dragonborn" },
+      { name: "Silver dragonborn" },
+      { name: "White dragonborn" },
+    ],
+  },
+};
--- src/utils.ts
+++ src/utils.ts
@@ -10,8 +10,13 @@ export enum Sizes {
   Gargantuan,
 }
 
+export const levels = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+
 export enum Languages {
   "Common" = "Common",
+  "Draconic" = "Draconic",
 }
 
 export enum Abilities {
